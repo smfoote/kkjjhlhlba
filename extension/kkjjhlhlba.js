cec9a19cd569a844ccf7c29a29617e61da1e3037
Prayrit Jain <prjain@prjain-ld.linkedin.biz>
2012-11-26 01:21:46 -0800

    fix indentation issues for real this time

--- extension/kkjjhlhlba.js
+++ extension/kkjjhlhlba.js
@@ -301,9 +301,9 @@ function Kkjjhlhlba() {
     for (var method in shortcuts) {
       if (shortcuts.hasOwnProperty(method)) {
         shortcut = shortcuts[method];
- 	if(currentElements >= maxElements) {
+        if(currentElements >= maxElements) {
           currentElements = 0;
-	  cheatsheet += '</ul><ul class="shortcuts">';
+          cheatsheet += '</ul><ul class="shortcuts">';
         }
         cheatsheet += '<li class="shortcut">' + method + ' <span class="description">' + shortcut.description + '<span></li>';
       }
