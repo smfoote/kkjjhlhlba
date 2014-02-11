1f166de706851dccbb33a4946f71fe40879721e1
Steven Foote <sfoote@linkedin.com>
2014-02-11 10:13:32 -0800

    Add an example HTML file and implementation file

---
+++ example/shortcuts.js
@@ -0,0 +1,11 @@
+kkjjhlhlba.start({
+  shortcuts: {
+    '/': {
+      description: 'search',
+      method: function() {
+        console.log('searching');
+      },
+      preventDefault: true
+    }
+  }
+});
